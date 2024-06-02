0ed0a6c351b3f11437259d51d6b0dc409e17cfc0
Alex <alexheppell@protonmail.com>
2024-06-02 16:12:21 +0100

    init project

---
+++ README.md
@@ -0,0 +1,6 @@
+# IWG-TEST
+A simple PIXI/TS game client implementing an instant win scratch card style game
+
+# BUID & SETUP
+To setup: npm i
+To build and run: npm run dev
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto'
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    static: './dist'
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
